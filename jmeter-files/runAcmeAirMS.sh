8413bde2ad444122c720f6182d850229101265c5
Joe McClure <jdmcclur@us.ibm.com>
2024-11-20 13:28:06 -0600

    Update runAcmeAirMS.sh

--- jmeter-files/runAcmeAirMS.sh
+++ jmeter-files/runAcmeAirMS.sh
@@ -4,14 +4,14 @@ THREADS=50
 
 sleep 5
 
-${JMETER_HOME}/bin/jmeter.sh -DusePureIDs=true -n -t AcmeAir-microservices-mpJwt-header.jmx -JHOST=${SUT} -JPORT=80 -JDURATION=10 -JTHREAD=1 -JRAMP=0
+${JMETER_HOME}/bin/jmeter.sh -DusePureIDs=true -n -t AcmeAir-microservices-mpJwt-header.jmx -JHOST=${SUT} -JPORT=80 -JDURATION=10 -JTHREAD=1 -JRAMP=0 -JUSER=999
 sleep 5
-${JMETER_HOME}/bin/jmeter.sh -DusePureIDs=true -n -t AcmeAir-microservices-mpJwt-header.jmx -JHOST=${SUT} -JPORT=80 -JDURATION=60 -JTHREAD=50 -JRAMP=0
+${JMETER_HOME}/bin/jmeter.sh -DusePureIDs=true -n -t AcmeAir-microservices-mpJwt-header.jmx -JHOST=${SUT} -JPORT=80 -JDURATION=60 -JTHREAD=${THREADS} -JRAMP=0 -JUSER=999
 sleep 5
-${JMETER_HOME}/bin/jmeter.sh -DusePureIDs=true -n -t AcmeAir-microservices-mpJwt-header.jmx -JHOST=${SUT} -JPORT=80 -JDURATION=180 -JTHREAD=50 -JRAMP=0
+${JMETER_HOME}/bin/jmeter.sh -DusePureIDs=true -n -t AcmeAir-microservices-mpJwt-header.jmx -JHOST=${SUT} -JPORT=80 -JDURATION=180 -JTHREAD=${THREADS} -JRAMP=0 -JUSER=999
 ./getFootprint.sh ${SUT}
 sleep 5
-${JMETER_HOME}/bin/jmeter.sh -DusePureIDs=true -n -t AcmeAir-microservices-mpJwt-header.jmx -JHOST=${SUT} -JPORT=80 -JDURATION=180 -JTHREAD=50 -JRAMP=0
+${JMETER_HOME}/bin/jmeter.sh -DusePureIDs=true -n -t AcmeAir-microservices-mpJwt-header.jmx -JHOST=${SUT} -JPORT=80 -JDURATION=180 -JTHREAD=${THREADS} -JRAMP=0 -JUSER=999
 sleep 5
-${JMETER_HOME}/bin/jmeter.sh -DusePureIDs=true -n -t AcmeAir-microservices-mpJwt-header.jmx -JHOST=${SUT} -JPORT=80 -JDURATION=300 -JTHREAD=50 -JRAMP=0
+${JMETER_HOME}/bin/jmeter.sh -DusePureIDs=true -n -t AcmeAir-microservices-mpJwt-header.jmx -JHOST=${SUT} -JPORT=80 -JDURATION=300 -JTHREAD=${THREADS} -JRAMP=0 -JUSER=999
 
